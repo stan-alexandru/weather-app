06ba05432856ee82489f9f602898e0859c9675a5
stan-alexandru <stan.alexandru26@gmail.com>
2024-02-03 18:08:02 +0200

    chore:switch to postcss

---
+++ prettier.config.js
@@ -0,0 +1,22 @@
+/** @type {import("prettier").Config} */
+
+export default {
+	printWidth: 80,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	quoteProps: "as-needed",
+	jsxSingleQuote: true,
+	trailingComma: "all",
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: "always",
+	requirePragma: false,
+	insertPragma: false,
+	proseWrap: "preserve",
+	htmlWhitespaceSensitivity: "css",
+	endOfLine: "lf",
+	embeddedLanguageFormatting: "auto",
+	singleAttributePerLine: false,
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./assets/style/index.scss";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './assets/style/index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>
+	</React.StrictMode>,
 );

7307035dd926741cce533ff1db85e412a4e7d743
stan-alexandru <stan.alexandru26@gmail.com>
2024-02-08 00:31:58 +0200

    feat:add basic logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,61 @@
+import { useState } from 'react';
+import type { OpenWeatherGeoCoding, OpenWeatherOneCall } from '@/types';
+import {
+	callOpenWeatherOneCall,
+	getBrowserCoordinates,
+	getOpenWeatherGeolocation,
+	getOpenWeatherReverseGeolocation,
+} from '@/lib/helpers';
+
 function App() {
+	const [search, setSearch] = useState<string>('');
+	const [cityArray, setCityArray] = useState<OpenWeatherGeoCoding[]>();
+
+	const [weather, setWeather] = useState<OpenWeatherOneCall>();
+	const [location, setLocation] = useState<OpenWeatherGeoCoding>();
+
+	async function handleForm(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		const location = await getOpenWeatherGeolocation({ location: search });
+		setCityArray(location);
+	}
+	async function handleCoordinates() {
+		const coords = await getBrowserCoordinates();
+		const geolocation = await getOpenWeatherReverseGeolocation({
+			lat: coords.latitude,
+			lon: coords.longitude,
+		});
+		const weather = await callOpenWeatherOneCall({
+			lat: coords.latitude,
+			lon: coords.longitude,
+		});
+
+		setWeather(weather);
+		setLocation(geolocation[0]);
+	}
+
 	return (
 		<>
-			<h1>Weather App</h1>
+			<form onSubmit={handleForm}>
+				<button onClick={handleCoordinates} type='button'>
+					Get Location
+				</button>
+				<input
+					type='search'
+					placeholder='Search'
+					value={search}
+					onChange={(event) => setSearch(event.target.value)}
+				/>
+				<button type='submit'>Submit</button>
+			</form>
+
+			{location && weather && (
+				<>
+					<h1>{location?.name}</h1>
+					<h1>{weather.lat}</h1>
+					<h1>{weather.current?.feels_like}</h1>
+				</>
+			)}
 		</>
 	);
 }
---
+++ src/lib/helpers.ts
@@ -0,0 +1,109 @@
+import type {
+	OpenWeatherGeoCoding,
+	OpenWeatherGeoCodingApi,
+	OpenWeatherOneCall,
+	OpenWeatherOneCallApi,
+	OpenWeatherReverseGeocodingApi,
+} from '@/types';
+
+export function getBrowserCoordinates() {
+	const myPromise: Promise<GeolocationCoordinates> = new Promise(
+		(resolve, reject) => {
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(
+					(position) => {
+						if (position) {
+							resolve(position.coords);
+						}
+					},
+					(error) => reject(error),
+				);
+			} else {
+				console.error('Geolocation is not supported by this browser.');
+			}
+		},
+	);
+	return myPromise;
+}
+
+export async function getOpenWeatherReverseGeolocation({
+	lat,
+	lon,
+	limit = 5,
+}: OpenWeatherReverseGeocodingApi): Promise<OpenWeatherGeoCoding[]> {
+	const BASE_URL = 'http://api.openweathermap.org/geo/1.0/reverse';
+	const url = new URL(BASE_URL);
+	url.searchParams.set('lat', lat.toString());
+	url.searchParams.set('lon', lon.toString());
+	url.searchParams.set('limit', limit.toString());
+	url.searchParams.set('appid', import.meta.env.VITE_APY_KEY);
+
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(
+				`Unable to fetch geocoding data ${(response.status, response.text)}`,
+			);
+		}
+		const results = await response.json();
+		return results as Promise<OpenWeatherGeoCoding[]>;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+}
+export async function getOpenWeatherGeolocation({
+	location,
+	limit = 5,
+}: OpenWeatherGeoCodingApi): Promise<OpenWeatherGeoCoding[]> {
+	const BASE_URL = 'http://api.openweathermap.org/geo/1.0/direct';
+	const url = new URL(BASE_URL);
+	url.searchParams.set('q', location);
+	url.searchParams.set('limit', limit.toString());
+	url.searchParams.set('appid', import.meta.env.VITE_APY_KEY);
+
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(
+				`Unable to fetch geocoding data ${(response.status, response.text)}`,
+			);
+		}
+		const results = await response.json();
+		return results as Promise<OpenWeatherGeoCoding[]>;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+}
+export async function callOpenWeatherOneCall({
+	lat,
+	lon,
+	units = 'metric',
+	lang = 'en',
+	exclude,
+}: OpenWeatherOneCallApi): Promise<OpenWeatherOneCall> {
+	const BASE_URL = 'https://api.openweathermap.org/data/3.0/onecall';
+	const url = new URL(BASE_URL);
+	url.searchParams.set('lat', lat.toString());
+	url.searchParams.set('lon', lon.toString());
+	url.searchParams.set('units', units);
+	url.searchParams.set('lang', lang);
+	exclude ? url.searchParams.set('exclude', exclude.join(',')) : null;
+	url.searchParams.set('appid', import.meta.env.VITE_APY_KEY);
+
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(
+				`Unable to fetch weather data ${(response.status, response.text)}`,
+			);
+		}
+		const results = await response.json();
+
+		return results as Promise<OpenWeatherOneCall>;
+	} catch (error) {
+		console.error(error);
+		throw error;
+	}
+}
